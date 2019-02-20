7d08261dd1d32013cbc7d77031eeeaecbf4ba402
Georgy <goosebumpsmail2@gmail.com>
2019-02-20 17:29:04 +0300

    first commit

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(Request $request, AuthenticationUtils $utils)
+    {
+        $error = $utils->getLastAuthenticationError();
+
+        $lastUserName = $utils->getLastUsername();
+        return $this->render('security/index.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUserName,
+        ]);
+    }
+
+    /**
+     * @Route("/add", name="add")
+     */
+    public function addToDataBase(UserPasswordEncoderInterface $encoder){
+
+        $user = new User();
+
+        $user->setUsername('goose');
+
+        $user->setPassword($encoder->encodePassword($user, '0000'));
+
+        $user->setEmail('test@mail.com');
+
+        $this->getDoctrine()->getManager()->persist($user);
+
+        $this->getDoctrine()->getManager()->flush();
+
+        return 'ok';
+
+    }
+}
